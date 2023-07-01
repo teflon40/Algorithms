ccd5d708609f352449d126f6200388cbee6cdf97
teflon40 <frimpong.bernard3220@gmail.com>
2023-07-01 17:43:25 +0000

    Documented the algorithm

--- C/euclid.c
+++ C/euclid.c
@@ -1,19 +1,6 @@
 /* Finding the greatest common divisor */
+
 int gcd(int m, int n)
-{
-	int hold;
-
-	while (n > 0)
-	{
-		hold = m % n;
-		m = n;
-		n = hold;
-	}
-	return m;
-}
-
-	/* OR */
-int gcd1(int m, int n)
 {
 	int hold;
 
@@ -25,7 +12,24 @@ int gcd1(int m, int n)
 			m = n;
 			n = hold;
 		}
-		m = m - n;
+		m = m % n;
 	}
 	return n;
 }
+
+/************************************************
+ * function gcd(m, n: integer): integer;	*
+ * 	var hold: integer;			*
+ * 	begin					*
+ * 	repeat					*
+ * 	   if m<n then				*
+ * 	       begin				*
+ * 	          hold:=m;			*
+ * 	          m:=n;				*
+ * 	          n:=hold			*
+ * 	       end;				*
+ * 	   m:=m%n				*
+ * 	   until m=0;				*
+ * 	gcd:=n;					*
+ * 	end;					*
+ ************************************************/
